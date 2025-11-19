23fe6ea0f221060fc083dc9f36e5881750508aed
NullString1 <kernd2005@gmail.com>
2025-11-19 10:46:51 +0000

    Workflows: Add deploy-backend to deploy on prod pushes

--- SourceCode/back-end/flake.nix
+++ SourceCode/back-end/flake.nix
@@ -15,12 +15,43 @@
         inherit system;
         crossSystem = {
           config = "aarch64-unknown-linux-gnu";
+          rust.rustcTarget = "aarch64-unknown-linux-gnu";
         };
       };
       rust = (pkgs.rust-bin.stable.latest.default.override {
         extensions = [ "rust-src" "rust-std" ];
         targets = [ "aarch64-unknown-linux-gnu" ];
       });
+      logSmartBackend = pkgsCross.rustPlatform.buildRustPackage {
+        pname = "logsmart-backend";
+        version = "latest";
+        src = ./.;
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+        };
+        installTargets = [ "logsmart-srv" ];
+        # buildPhase = "cargo build --release --target aarch64-unknown-linux-gnu --locked --offline";
+        nativeBuildInputs = [ pkgs.pkg-config pkgs.openssl ];
+        buildInputs = [ pkgsCross.openssl ];
+        SWAGGER_UI_DOWNLOAD_URL = "file://" + pkgs.fetchurl {
+          url = "https://github.com/swagger-api/swagger-ui/archive/refs/tags/v5.17.14.zip";
+          hash = "sha256-SBJE0IEgl7Efuu73n3HZQrFxYX+cn5UU5jrL4T5xzNw=";
+        };
+        HOST_CC = "${pkgs.stdenv.cc}/bin/cc";
+        OPENSSL_DIR = "${pkgs.openssl.out}";
+        OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+        AARCH64_UNKNOWN_LINUX_GNU_OPENSSL_DIR = "${pkgsCross.openssl.out}";
+        AARCH64_UNKNOWN_LINUX_GNU_OPENSSL_LIB_DIR = "${pkgsCross.openssl.out}/lib";
+        AARCH64_UNKNOWN_LINUX_GNU_OPENSSL_INCLUDE_DIR = "${pkgsCross.openssl.dev}/include";
+        PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+        PKG_CONFIG_PATH_aarch64_unknown_linux_gnu = "${pkgsCross.openssl.dev}/lib/pkgconfig";
+        PKG_CONFIG_ALLOW_CROSS = 1;
+        PKG_CONFIG_SYSROOT_DIR = "${pkgsCross.stdenv.cc.libc}";
+      };
+      packages = {
+        aarch64-linux = logSmartBackend;
+      };
     in
     {
       devShells.default = pkgs.mkShell {
@@ -31,6 +62,7 @@
           pkgsCross.openssl.dev
           libz
           pkg-config
+          sqlx-cli
         ];
         PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
         PKG_CONFIG_PATH_aarch64_unknown_linux_gnu = "${pkgsCross.openssl.dev}/lib/pkgconfig";
@@ -45,6 +77,7 @@
         CXX_aarch64_unknown_linux_gnu = "${pkgsCross.stdenv.cc}/bin/aarch64-unknown-linux-gnu-g++";
         AR_aarch64_unknown_linux_gnu = "${pkgsCross.stdenv.cc}/bin/aarch64-unknown-linux-gnu-ar";
       };
+      packages = packages;
     }
   );
 }
