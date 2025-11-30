ab6efdca09587e67caf0860ae622667be60aee1c
NullString1 <kernd2005@gmail.com>
2025-11-30 15:35:18 +0000

    Both: Update flake nixpkgs version, will aid caching

--- SourceCode/front-end/flake.nix
+++ SourceCode/front-end/flake.nix
@@ -2,11 +2,11 @@
   description = "SvelteKit development flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
