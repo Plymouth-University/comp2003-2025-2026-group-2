c6b346b1673c6577de2d8b1f9dfcf50cd412cb1d
NullString1 <kernd2005@gmail.com>
2025-12-07 18:21:32 +0000

    Testing: Add e2e test

---
+++ SourceCode/front-end/logsmart/tests/login_admin.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('login_admin', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  await page.getByRole('textbox', { name: 'Email' }).fill('testuser2@logsmart.app');
+  await page.getByRole('textbox', { name: 'Email' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Password' }).fill('Test123!');
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.waitForURL('**/dashboard');
+  await expect(page.locator('span')).toContainText('testuser2@logsmart.app');
+  await expect(page.locator('body')).toContainText('Test User');
+  await expect(page.locator('body')).toContainText('testuser2@logsmart.app');
+  await expect(page.locator('body')).toContainText('TestCompany1');
+  await expect(page.locator('body')).toContainText('admin');
+});
