98cdd243957b76ba9e9a45d4fc52bed4b1322572
NullString1 <kernd2005@gmail.com>
2026-04-20 12:34:39 +0100

    bugfix: duplicate code

--- SourceCode/front-end/logsmart/src/routes/(authenticated)/template-designer/DesignCanvas.svelte
+++ SourceCode/front-end/logsmart/src/routes/(authenticated)/template-designer/DesignCanvas.svelte
@@ -52,7 +52,6 @@
 	let snapLines = $state<{ x: number[]; y: number[] }>({ x: [], y: [] });
 	let isDragging = $state(false);
 	let snapEnabled = $state(true);
-	let canvasHeight = $state<number>(500);
 
 	function selectItem(id: string) {
 		selectedItemId = id;
